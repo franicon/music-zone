32c62d284b7443cd0a67a63371787712357a6ef5
Abayomi Aluko <franicons@gmail.com>
2022-12-24 11:15:30 +0100

    feat: development
    * Playing Music

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,12 +1,18 @@
+import { Howl } from "howler";
 import { defineStore } from "pinia";
 
 export default defineStore("player", {
   state: () => ({
+    sounds: {},
     current_song: {},
   }),
   actions: {
     async newSong(song) {
       this.current_song = song;
+      this.sounds = new Howl({
+        src: [song.url],
+        html5: true,
+      });
     },
   },
 });
